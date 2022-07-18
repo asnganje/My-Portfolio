2faf35ca02841a6ef5cda92dc050dfba156960a9
asnganje <abd.nganje@gmail.com>
2022-07-18 22:31:04 +0300

    modified index.html, myScript.js and styles.css

--- myScript.js
+++ myScript.js
@@ -1,10 +1,10 @@
 function myFunction() {
-  let x = document.querySelector('.topbar');
-  let btn = document.getElementById('hamburger-menu');
+  const x = document.querySelector('.topbar');
+  const btn = document.getElementById('hamburger-menu');
 
-  if (x.className === "topbar") {
+  if (x.className === 'topbar') {
     x.classList.add('visible');
-    btn.innerHTML = "&#10006;";
+    btn.innerHTML = '&#10006;';
   }
   else {
     x.classList.remove('visible');
@@ -12,3 +12,4 @@ function myFunction() {
   }
 
 }
+return myFunction();
