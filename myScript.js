860c471e2f004b1bc7e09357f2c35b4ad7efc8cd
Lucas Comamala <lucascomamala@gmail.com>
2022-07-18 11:30:43 -0600

    Revert "Add mobile menu"
    This reverts commit 531dcacb508365162f25ab5d905e9fcafb047ff9.

--- myScript.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  let x = document.querySelector('.topbar')
-  if (x.className === "topbar") {
-    x.classList.add('visible');
-  }
-  else {
-    x.classList.remove('visible');
-  }
-}
