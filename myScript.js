a1078eb3feb67cdf12a394b0d25871765e1f5ea6
asnganje <abd.nganje@gmail.com>
2022-07-18 22:45:57 +0300

    modified myScript.js

--- myScript.js
+++ myScript.js
@@ -2,14 +2,11 @@ function myFunction() {
   const x = document.querySelector('.topbar');
   const btn = document.getElementById('hamburger-menu');
 
-if (x.className === 'topbar') {
+  if (x.className === 'topbar') {
     x.classList.add('visible');
-    btn.innerHTML = '&#10006;';
-                              }
+    btn.innerHTML = '&#10006;';}
   else 
-{
+  {
     x.classList.remove('visible');
-    btn.innerHTML = '&#9776;';
+    btn.innerHTML = '&#9776;';}
 }
-}
-
