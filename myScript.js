385172cfbf448d52fba6cf6d870ab59876913e91
asnganje <abd.nganje@gmail.com>
2022-07-18 22:53:02 +0300

    modified myscript to fix linter errors

--- myScript.js
+++ myScript.js
@@ -2,7 +2,6 @@ function myFunction() {
   const x = document.querySelector('.topbar');
   const btn = document.getElementById('hamburger-menu');
 
-  if (x.className === 'topbar') {x.classList.add('visible'); btn.innerHTML = '&#10006'; }
-  
-  else {x.classList.remove('visible'); btn.innerHTML = '&#9776'; }
+  if (x.className === 'topbar') { x.classList.add('visible'); btn.innerHTML = '&#10006'; }
+  else { x.classList.remove('visible'); btn.innerHTML = '&#9776'; }
 }
