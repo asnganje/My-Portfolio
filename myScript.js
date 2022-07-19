32d4c66afe7c69a02bbd0c01c116889d23a13a1a
asnganje <abd.nganje@gmail.com>
2022-07-19 18:42:31 +0300

    modified myScript.js

--- myScript.js
+++ myScript.js
@@ -1,10 +1,17 @@
-/* eslint-disable no-unused-vars */
-function myFunction() {
-  const x = document.querySelector('.topbar');
-  const btn = document.getElementById('hamburger-menu');
 
-  if (x.className === 'topbar') { x.classList.add('visible'); btn.innerHTML = '&#10006'; } else {
-    x.classList.remove('visible'); btn.innerHTML = '&#9776';
-  }
+function myFunction(){
+  const hamburg = document.querySelector('.hamburger-menu');
+  const top = document.querySelector('.topbar');
+if (hamburg.className === 'hamburger-menu') 
+{hamburg.classList.add('visible'); hamburg.innerHTML = '&#10006'; } 
+
+if (top.className === 'topbar') 
+{ top.classList.add('visible');} 
+
+else {
+  hamburg.classList.remove('visible'); hamburg.innerHTML = '&#9776';
+  hamburg.classList.toggle('visible');
 }
-/* eslint-disable no-unused-vars */
+
+}
+
