dd21823dd966e9dd8c7b6f36a30d8946cc32e4a3
asnganje <abd.nganje@gmail.com>
2022-07-22 19:08:14 +0300

    added portfolio repo

--- validation.js
+++ validation.js
@@ -8,3 +8,27 @@ thisForm.addEventListener('submit', (e) => {
     e.preventDefault(); const info = 'Sorry! Email must be written in small letters only.'; safer.innerHTML = info;
   }
 });
+const storeName = document.getElementById('fullName');
+const storeEmail = document.getElementById('email');
+const infoS = document.getElementById('her');
+
+function populateStore () {
+  const dataStorageObject = {
+    name: storeName.value,
+    email: storeEmail.value,
+    msg: infoS.value,
+  };
+
+  localStorage.setItem('data', JSON.stringify(dataStorageObject));
+}
+
+storeName.addEventListener('input', populateStore);
+storeEmail.addEventListener('input', populateStore);
+infoS.addEventListener('input', populateStore);
+
+window.addEventListener('load', () => {
+  const capturedData = JSON.parse(localStorage.getItem('data'));
+  storeName.value = capturedData.name;
+  storeEmail.value = capturedData.email;
+  infoS.value = capturedData.msg;
+});
